a0116dc5f6dda55d05aad0fe33f0fd4be42037cb
tatiana <taatt2000@gmail.com>
2024-10-11 11:42:59 +0200

    +

---
+++ src/constant/users.js
@@ -0,0 +1,1 @@
+import { v4 } from 'uuid';
---
+++ src/user/User.jsx
@@ -0,0 +1,14 @@
+const User = () => {
+    return (
+        <>
+        <img src="" alt="" />
+        <h1></h1>
+        <p></p>
+        <p></p>
+        <div>
+            <button></button>
+            <button></button>
+        </div>
+        </>
+    )
+}
